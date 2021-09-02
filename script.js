86af6573ef40552e53b8c9043a655dd35356d0c7
aru99 <mohammadarmanmavric@gmail.com>
2021-09-02 20:37:15 +0530

    Both senarios, win as well as reset implemented

--- script.js
+++ script.js
@@ -14,20 +14,32 @@
 // console.log(document.querySelector('.guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20 + 1);
-document.querySelector('.number').textContent = secretNumber;
+
 let scoreValue = 20;
 
-//note: eventlistner for button click start
+//note: eventlistner for Check button start========================
 document.querySelector('.check').addEventListener('click', function () {
   //note: when ever we retrive anything from the user Interface it is of type string, thus changeing the type as per the requirement
   const guessNumber = Number(document.querySelector('.guess').value);
   console.log(guessNumber, typeof guessNumber);
 
+  //when there is no input
   if (!guessNumber) {
     document.querySelector('.message').textContent = 'No number! 👀';
-  } else if (guessNumber === secretNumber) {
+  }
+  //when the player wins
+  else if (guessNumber === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
-  } else if (guessNumber > secretNumber) {
+    //displaying the correctnumber when the player wins
+    document.querySelector('.number').textContent = secretNumber;
+
+    //changing the background color of the screen when the player wins
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    //increasing the width of the main number
+    document.querySelector('.number').style.width = '30rem';
+  }
+  //when the input is higher than the number
+  else if (guessNumber > secretNumber) {
     if (scoreValue > 1) {
       document.querySelector('.message').textContent = 'Too High📈';
       scoreValue--;
@@ -36,7 +48,9 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game';
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guessNumber < secretNumber) {
+  }
+  //when the input is lower than the nnumber
+  else if (guessNumber < secretNumber) {
     if (scoreValue > 1) {
       document.querySelector('.message').textContent = 'Too Low📉';
       scoreValue--;
@@ -47,3 +61,24 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+//note: eventlistner for Check button end========================
+
+//note: eventlistner for again button start========================
+//task:
+// -- on click of again button following should happen---------
+//     a) The background color of the screen should go back to the initial color : done
+//     b) Correct number should dissaper and "?" should be there : done
+//     c)reset the correct number message back to start guessing : done
+//     d)reset score : done
+//     e)empty out the input field : done
+
+document.querySelector('.again').addEventListener('click', function () {
+  scoreValue = 20;
+  document.querySelector('.score').textContent = scoreValue;
+  console.log(scoreValue);
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'start guessing...';
+  document.querySelector('.guess').value = null;
+});
+//note: eventlistner for again button end========================
